7a35ffcf73407016b21be0c8ba6ed0a50ea0cae6
Иван <lolAHOHIMlol@mail.ru>
2024-08-10 20:50:05 +0300

    Добавьте файлы проекта.

---
+++ 4asikii/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace _4asikii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4TimeNow = new System.Windows.Forms.Timer(this.components);
+            this.label1TimeNow = new System.Windows.Forms.Label();
+            this.labelVremya = new System.Windows.Forms.Label();
+            this.buttonBudStop = new System.Windows.Forms.Button();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.buttonBudDop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(527, 488);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1100;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 61000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 3660000;
+            // 
+            // timer4TimeNow
+            // 
+            this.timer4TimeNow.Tick += new System.EventHandler(this.timer4TimeNow_Tick);
+            // 
+            // label1TimeNow
+            // 
+            this.label1TimeNow.AutoSize = true;
+            this.label1TimeNow.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1TimeNow.Location = new System.Drawing.Point(383, 299);
+            this.label1TimeNow.Name = "label1TimeNow";
+            this.label1TimeNow.Size = new System.Drawing.Size(58, 22);
+            this.label1TimeNow.TabIndex = 2;
+            this.label1TimeNow.Text = "label1";
+            // 
+            // labelVremya
+            // 
+            this.labelVremya.AutoSize = true;
+            this.labelVremya.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelVremya.Location = new System.Drawing.Point(621, 157);
+            this.labelVremya.Name = "labelVremya";
+            this.labelVremya.Size = new System.Drawing.Size(78, 36);
+            this.labelVremya.TabIndex = 25;
+            this.labelVremya.Text = "label";
+            // 
+            // buttonBudStop
+            // 
+            this.buttonBudStop.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonBudStop.Enabled = false;
+            this.buttonBudStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBudStop.Location = new System.Drawing.Point(594, 258);
+            this.buttonBudStop.Name = "buttonBudStop";
+            this.buttonBudStop.Size = new System.Drawing.Size(136, 32);
+            this.buttonBudStop.TabIndex = 24;
+            this.buttonBudStop.Text = "Убрать";
+            this.buttonBudStop.UseVisualStyleBackColor = false;
+            this.buttonBudStop.Click += new System.EventHandler(this.buttonBudStop_Click);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskedTextBox2.Location = new System.Drawing.Point(617, 154);
+            this.maskedTextBox2.Mask = "00:00";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(82, 41);
+            this.maskedTextBox2.TabIndex = 23;
+            this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            // 
+            // buttonBudDop
+            // 
+            this.buttonBudDop.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonBudDop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBudDop.Location = new System.Drawing.Point(594, 220);
+            this.buttonBudDop.Name = "buttonBudDop";
+            this.buttonBudDop.Size = new System.Drawing.Size(136, 32);
+            this.buttonBudDop.TabIndex = 22;
+            this.buttonBudDop.Text = "Добавить";
+            this.buttonBudDop.UseVisualStyleBackColor = false;
+            this.buttonBudDop.Click += new System.EventHandler(this.buttonBudDop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(588, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 36);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Будильник";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(615, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Время (ч:м)";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(563, 44);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 512);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelVremya);
+            this.Controls.Add(this.buttonBudStop);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.buttonBudDop);
+            this.Controls.Add(this.label1TimeNow);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Часы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4TimeNow;
+        private System.Windows.Forms.Label label1TimeNow;
+        private System.Windows.Forms.Label labelVremya;
+        private System.Windows.Forms.Button buttonBudStop;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button buttonBudDop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ 4asikii/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _4asikii.Properties;
+using System.Drawing.Drawing2D;
+using System.Media;
+using System.Runtime.InteropServices;
+
+namespace _4asikii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        int fff = 0;
+        int xc, yc, x, y, r = 160;
+        Graphics gr;
+        double ang =0, dangle = 2 * Math.PI / 60;
+        Pen pen = new Pen(Color.Red);
+
+        int xc1, yc1, x1, y1, r1 = 150;
+        Graphics gr1;
+        double ang1 = 0, dangle1 = 2 * Math.PI / 60;
+        Pen pen1 = new Pen(Color.Blue);
+
+        int xc2, yc2, x2, y2, r2 = 130;
+        Graphics gr2;
+        double ang2 = 0, dangle2 = 2 * Math.PI / 60;
+        Pen pen2 = new Pen(Color.Black);
+
+        Graphics gr3;
+        int w = 0, h = 0;
+        string s,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12;
+        Font f = new Font("Arial", 30, FontStyle.Italic);
+        SolidBrush br = new SolidBrush(Color.Black);
+        StringFormat sf = new StringFormat();
+        PointF p = new PointF();
+        
+
+        private void buttonBudStop_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ваш будильник был остановлен!");
+            buttonBudStop.Enabled = false;
+            buttonBudDop.Enabled = true;
+            maskedTextBox2.Text = "";
+            maskedTextBox2.Enabled = true;
+            maskedTextBox2.Visible = true;
+            labelVremya.Visible = false;
+        }
+
+        private void buttonBudDop_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ваш будильник установлен!" + Environment.NewLine + Environment.NewLine + "Проверьте правильность поставленого времени, чтобы избежать ошибок");
+            labelVremya.Text = maskedTextBox2.Text;
+            maskedTextBox2.Visible = false;
+            labelVremya.Visible = true;
+            buttonBudStop.Enabled = true;
+            buttonBudDop.Enabled = false;
+            maskedTextBox2.Enabled = false;
+        }
+
+        private void timer4TimeNow_Tick(object sender, EventArgs e)
+        {
+            label1TimeNow.Text = DateTime.Now.Hour.ToString("00") + ":" + DateTime.Now.Minute.ToString("00") + ":" + DateTime.Now.Second.ToString("00");
+
+            if (dateTimePicker1.Value.Date == DateTime.Today && label1TimeNow.Text == labelVremya.Text + ":00")
+            {
+                SoundPlayer BudStop = new SoundPlayer(@"C:\Windows\Media\Alarm01.wav");
+                maskedTextBox2.Text = "";
+                labelVremya.Visible = false;
+                maskedTextBox2.Visible = true;
+                buttonBudDop.Enabled = true;
+                buttonBudStop.Enabled = false;
+                BudStop.Play();
+                maskedTextBox2.Enabled = true;
+                fff++;
+                if (fff == 1)
+                {
+                    MessageBox.Show("Будильник: *Звенит* ");
+                }
+                
+            }
+            
+
+        
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            ang1 += dangle1;
+            pictureBox1.Invalidate();
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            ang2 += dangle2;
+            pictureBox1.Invalidate();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskedTextBox2.Text = "";
+            maskedTextBox2.Visible = true;
+            labelVremya.Visible = false;
+
+            timer4TimeNow.Start();
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+
+            pen.Width = 2;
+            this.pictureBox1.Image = Resources.Chiferblat;
+            xc = pictureBox1.Width / 2;
+            yc = pictureBox1.Height / 2;
+            gr = pictureBox1.CreateGraphics();
+
+            pen1.Width = 2;
+            xc1 = pictureBox1.Width / 2;
+            yc1 = pictureBox1.Height / 2;
+            gr1 = pictureBox1.CreateGraphics();
+
+            pen2.Width = 4;
+            pen2.StartCap = LineCap.Square;
+            pen2.EndCap = LineCap.ArrowAnchor;
+            xc2 = pictureBox1.Width / 2;
+            yc2 = pictureBox1.Height / 2;
+            gr2 = pictureBox1.CreateGraphics();
+
+            w = this.ClientSize.Width;
+            h = this.ClientSize.Height;
+            s = "12"; s1 = "1"; s2 = "2"; s3 = "3"; s4 = "4"; s5 = "5";
+            s6 = "6"; s7 = "7"; s8 = "8"; s9 = "9"; s10 = "10"; s11 = "11";
+
+            SizeF size = gr.MeasureString(s, f);
+
+            textBox1.Text = dateTimePicker1.Value.Date.ToString();
+            textBox2.Text = DateTime.Today.ToString();
+        }
+        int aaa = int.Parse(DateTime.Now.Second.ToString("00"));
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int sec = DateTime.Now.Second;
+            int[] handCoord = new int[2];
+            handCoord = msCoord(sec, 190);
+
+            Graphics gr = e.Graphics;
+            x = xc + (int)(r * Math.Sin(ang));
+            y = yc - (int)(r * Math.Cos(ang));
+            gr.DrawLine(pen, xc, yc, handCoord[0], handCoord[1]);
+
+            Graphics gr1 = e.Graphics;
+            x1 = xc1 + (int)(r1 * Math.Sin(ang1));
+            y1 = yc1 - (int)(r1 * Math.Cos(ang1));
+            gr1.DrawLine(pen1, xc1, yc1, x1, y1);
+
+            Graphics gr2 = e.Graphics;
+            x2 = xc2 + (int)(r2 * Math.Sin(ang2));
+            y2 = yc2 - (int)(r2 * Math.Cos(ang2));
+            gr2.DrawLine(pen2, xc2, yc2, x2, y2);
+
+            Graphics gr3 = e.Graphics;
+            gr3.DrawString(s, f, br, 230, 40, sf); gr3.DrawString(s6, f, br, 242, 400, sf); gr3.DrawString(s3, f, br, 430, 220, sf); gr3.DrawString(s9, f, br, 50, 220, sf);
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ang += dangle;
+            pictureBox1.Invalidate();
+             
+        }
+
+
+        private int[] msCoord(int grad, int r)
+        {
+            int[] coord = new int[2];
+            grad *= 6;
+
+            if (grad >= 0 && grad <= 180)
+            {
+                coord[0] = xc + (int)(r * Math.Sin(Math.PI * grad / 180));
+                coord[1] = yc - (int)(r * Math.Cos(Math.PI * grad / 180));
+            }
+            else
+            {
+                coord[0] = xc - (int)(r * -Math.Sin(Math.PI * grad / 180));
+                coord[1] = yc - (int)(r * Math.Cos(Math.PI * grad / 180));
+            }
+            return coord;
+        }
+
+
+
+    }
+}
